81a627c529e48fc383878244104a1fe8aec583af
AnaM93 <ana.18_03@yahoo.com>
2024-02-19 13:49:11 -0800

    started the CoffeMasters project using Vanilla JS

---
+++ app.js
@@ -0,0 +1,10 @@
+import Store from "./services/Store";
+import API from "./services/API";
+import { loadData } from "./services/Menu";
+
+window.app = {};
+app.store = Store;
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadData();
+});
